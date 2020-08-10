a1ecad2c9ba797f6ef7de39cbe2ce52d1872757c
JosueEMG <josueomega355@gmail.com>
2020-08-09 22:22:08 -0500

    Video 19. Buscador dinamico en una tabla para laboratorios

--- controlador/UsuarioController.php
+++ controlador/UsuarioController.php
@@ -120,7 +120,7 @@ if($_POST['funcion']=='crear_usuario'){
     $dni = $_POST['dni'];
     $pass = $_POST['pass'];
     $tipo = 2;
-    $avatar='avatar.png';
+    $avatar='default.png';
     $usuario->crear($nombre,$apellido,$edad,$dni,$pass,$tipo,$avatar);
 }
 if($_POST['funcion']=='ascender'){
