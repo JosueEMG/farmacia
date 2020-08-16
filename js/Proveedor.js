4295fc6e5796f3ed7859bf0552ff6fb10c40fa52
Jp-dsouls <werras04@gmail.com>
2020-08-15 21:18:18 -0500

    Video 32
    El boton eliminar no agarra :,v

--- js/Proveedor.js
+++ js/Proveedor.js
@@ -89,5 +89,88 @@ $(document).ready(function(){
         $('#nombre_logo').html(nombre);
         $('#id_logo_prov').val(id);
         $('#funcion').val(funcion);
+        $('#avatar').val(avatar);
     });
+    $('#form-logo').submit(e=>{
+      let formData = new FormData($('#form-logo')[0]);
+      $.ajax({
+        url:'../controlador/ProveedorController.php',
+        type: 'POST',
+        data: formData,
+        cache: false,
+        processData: false,
+        contentType: false,
+      }).done(function(response){
+        const json = JSON.parse(response);
+        if(json.alert=='edit'){
+          $('#logoactual').attr('src',json.ruta);
+          $('#edit-prov').hide('slow');
+          $('#edit-prov').show(1000);
+          $('#edit-prov').hide(2000);
+          $('#form-logo').trigger('reset');
+          buscar_prov();
+        }
+        else{
+          $('#noedit-prov').hide('slow');
+          $('#noedit-prov').show(1000);
+          $('#noedit-prov').hide(2000);
+          $('#form-logo').trigger('reset');
+        }
+      });
+      e.preventDefault();
+    });
+
+    $(document).on('click', '.borrar', (e)=>{
+      funcion="borrar";
+      const elemento = $(this)[0].activeElement.parentElement.parentElement.parentElement.parentElement;
+      const id = $(elemento).attr('provId');
+      const nombre = $(elemento).attr('provNombre');;
+      const avatar = $(elemento).attr('provAvatar');; 
+
+      const swalWithBootstrapButtons = Swal.mixin({
+          customClass: {
+            confirmButton: 'btn btn-success',
+            cancelButton: 'btn btn-danger mr-1'
+          },
+          buttonsStyling: false
+        })
+        
+        swalWithBootstrapButtons.fire({
+          title: 'Desea eliminar '+nombre+'?',
+          text: "No se podra revertir esto!",
+          imageUrl: ''+avatar+'',
+          imageWhidth: 100, 
+          imageHeight: 100,
+          showCancelButton: true,
+          confirmButtonText: 'Si, eliminar esto!',
+          cancelButtonText: 'No, cancelar!',
+          reverseButtons: true
+        }).then((result) => {
+          if (result.value) {
+              $.post('../controlador/ProveedorController.php',{id,funcion},(response)=>{
+                  if(response=='borrado'){
+                      swalWithBootstrapButtons.fire(
+                          'Borrado!',
+                          'El proveedor '+nombre+' fue borrado',
+                          'success'
+                        )
+                        buscar_prov();
+                  }else{
+                      swalWithBootstrapButtons.fire(
+                          'No se pudo borrar',
+                          'El proveedor '+nombre+' no fue borrado debido a que esta siendo usado por un producto',
+                          'error'
+                        )
+                  }
+              })
+            
+          } else if (result.dismiss === Swal.DismissReason.cancel) {
+            swalWithBootstrapButtons.fire(
+              'Cancelado',
+              'El proveedor '+nombre+' no fue borrado',
+              'error'
+            )
+          }
+        })
+  })
 });
