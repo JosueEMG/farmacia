b6eee366a45728dac614afc844d93d4a98b8a1fa
Jp-dsouls <werras04@gmail.com>
2020-08-18 00:56:05 -0500

    Video 41
    Carrito de compras con Local Storage

--- js/Carrito.js
+++ js/Carrito.js
@@ -1,4 +1,5 @@
 $(document).ready(function(){
+  RecuperarLS_carrito();
     $(document).on('click','.agregar-carrito',(e)=>{
       const elemento =$(this)[0].activeElement.parentElement.parentElement.parentElement.parentElement;
       const id= $(elemento).attr('prodId');
@@ -22,24 +23,94 @@ $(document).ready(function(){
           avatar: avatar,
           cantidad:1
       }
-      template=`
-        <tr>
+      let id_producto;
+      let productos;
+      productos=RecuperarLS();
+      productos.forEach(prod => {
+        if (prod.id===producto.id) {
+          id_producto=prod.id;
+        }
+      });
+      if (id_producto===producto.id) {
+        Swal.fire({
+          icon: 'error',
+          title: 'Oops...',
+          text: 'El producto ya existe!',
+        })
+      }
+      else{
+        template=`
+          <tr prodId="${producto.id}">
             <td>${producto.id}</td>
             <td>${producto.nombre}</td>
             <td>${producto.concentracion}</td>
             <td>${producto.adicional}</td>
             <td>${producto.precio}</td>
             <td><button class="borrar-producto btn btn-danger"><i class="fas fa-times-circle"></i></button></td>
-        </tr>
-      `;
-      $('#lista').append(template);
+          </tr>
+        `;
+        $('#lista').append(template);
+        AgregarLS(producto);
+      }
+
+      
     })
     $(document).on('click','.borrar-producto',(e)=>{
         const elemento =$(this)[0].activeElement.parentElement.parentElement;
+        const id=$(elemento).attr('prodId');
         elemento.remove();
+        Eliminar_producto_LS(id);
+
      })
      $(document).on('click','#vaciar-carrito',(e)=>{
       $('#lista').empty();
-      e.preventDefault();
+      EliminarLS();
    })
+   function RecuperarLS(){
+     let productos;
+     if(localStorage.getItem('productos')===null){
+      productos=[];
+     }
+     else{
+       productos=JSON.parse(localStorage.getItem('productos'));
+     }
+     return productos;
+   }
+   function AgregarLS(producto){
+     let productos;
+     productos=RecuperarLS();
+     productos.push(producto);
+     localStorage.setItem('productos',JSON.stringify(productos))
+
+   }
+   function RecuperarLS_carrito(){
+    let productos;
+    productos=RecuperarLS();
+    productos.forEach(producto=>{
+      template=`
+          <tr prodId="${producto.id}">
+            <td>${producto.id}</td>
+            <td>${producto.nombre}</td>
+            <td>${producto.concentracion}</td>
+            <td>${producto.adicional}</td>
+            <td>${producto.precio}</td>
+            <td><button class="borrar-producto btn btn-danger"><i class="fas fa-times-circle"></i></button></td>
+          </tr>
+        `;
+        $('#lista').append(template);
+    });
+   }
+   function Eliminar_producto_LS(id){
+    let productos;
+    productos=RecuperarLS();
+    productos.forEach(function(producto,indice){
+      if (producto.id===id) {
+        productos.splice(indice,1);
+      }      
+    });
+    localStorage.setItem('productos',JSON.stringify(productos))
+   }
+   function EliminarLS() {
+     localStorage.clear();
+   }
 })
