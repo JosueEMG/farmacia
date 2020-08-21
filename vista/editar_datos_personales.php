44387aef07789e6d49ce8c8389b2dac2f6e8ab04
Jp-dsouls <werras04@gmail.com>
2020-08-21 00:18:04 -0500

    video 61
    ultimos cambios

--- vista/editar_datos_personales.php
+++ vista/editar_datos_personales.php
@@ -102,7 +102,7 @@ if ($_SESSION['us_tipo'] == 1 || $_SESSION['us_tipo'] == 3 || $_SESSION['us_tipo
     <div class="content-wrapper">
         <!-- Content Header (Page header) -->
         <section class="content-header">
-            <div class="container-fluid">S
+            <div class="container-fluid">
                 <div class="row mb-2">
                     <div class="col-sm-6">
                         <h1>Datos Personales</h1>
